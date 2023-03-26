5fe379770e570b96729cebb9b292ac8e2433a574
Rohit Sidhwani <rohitsidhwani@Rohits-Air.Dlink>
2023-03-26 15:49:28 +0530

    Header Middle Search Component Completed.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { FC } from "react";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 const Header:FC = function () {
     return (
@@ -13,7 +14,10 @@ const Header:FC = function () {
             </div>
 
             {/* Middle - Search */}
-            <div></div>
+            <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+                <input type="text" placeholder="Start your search" className="pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400" />
+                <MagnifyingGlassIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
+            </div>
 
             {/* Right */}
             <div></div>
