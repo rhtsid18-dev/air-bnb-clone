007350c440b85bec67d04912492b4274b91540a8
Rohit Sidhwani <rohitsidhwani@Rohits-Air.Dlink>
2023-03-26 14:07:38 +0530

    Added responsiveness to Header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 
 const Header:FC = function () {
     return (
-        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5">
+        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
             <div className="relative flex items-center h-10 cursor-pointer my-auto">
                 <Image 
                     src="https://links.papareact.com/qd3" 
@@ -12,7 +12,7 @@ const Header:FC = function () {
                     style={{ objectFit: "contain", objectPosition: "left" }} />
             </div>
 
-            {/* Middle */}
+            {/* Middle - Search */}
             <div></div>
 
             {/* Right */}
