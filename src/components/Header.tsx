dda5215b036fe8fd39bf51cdd83933574205cae2
Rohit Sidhwani <rhtsid18.dev@gmail.com>
2023-03-26 15:54:19 +0530

    Removed placholder comment.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,6 @@ const Header:FC = function () {
                     style={{ objectFit: "contain", objectPosition: "left" }} />
             </div>
 
-            {/* Middle - Search */}
             <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
                 <input type="text" placeholder="Start your search" className="pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400" />
                 <MagnifyingGlassIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
